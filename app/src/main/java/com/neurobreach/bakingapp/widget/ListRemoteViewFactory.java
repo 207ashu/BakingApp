faf2039ceddc0e19d6608a2e20910bd94d861244
207ashu <207ashu@gmail.com>
2018-10-09 13:16:50 +0530

    Initial commit

---
+++ app/src/main/java/com/neurobreach/bakingapp/widget/ListRemoteViewFactory.java
@@ -0,0 +1,73 @@
+package com.neurobreach.bakingapp.widget;
+
+import android.content.Context;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.neurobreach.bakingapp.R;
+import com.neurobreach.bakingapp.RecipeDetailActivity;
+import com.neurobreach.bakingapp.model.IngredientsModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListRemoteViewFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    private Context mContext;
+    private List<IngredientsModel> modelList = new ArrayList<IngredientsModel>();
+
+    public ListRemoteViewFactory(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        modelList = RecipeDetailActivity.ingredientsModelList;
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        if (modelList == null) return 0;
+        return modelList.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int i) {
+        RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.widget_list_view_item);
+        remoteViews.setTextViewText(R.id.widget_list_view_text_ingredient, modelList.get(i).getIngredient());
+        remoteViews.setTextViewText(R.id.widget_list_view_text_quantity, modelList.get(i).getQuantity() + "");
+        remoteViews.setTextViewText(R.id.widget_list_view_text_measure, modelList.get(i).getMeasure());
+        return remoteViews;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
